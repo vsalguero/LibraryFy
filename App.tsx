b92972b5e97652fea756374b7e14b1cb182f1d93
Vladimir Salguero <vladimir891@hotmail.com>
2022-02-17 13:32:14 -0600

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  <SafeAreaView>
+    <View style={styles.container}>
+      <Text>Hola mundo</Text>
+    </View>
+  </SafeAreaView>
+    
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E3E3E3',
+    padding: 5,
+  }
+});
+
+export default App;
